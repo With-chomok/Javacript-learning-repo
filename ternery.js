4a5537d33b5d32474af95af1fb997220390c5791
Chomok Das <183892208+With-chomok@users.noreply.github.com>
2025-07-29 13:01:22 +0600

    ternery operatory practice

---
+++ ternery.js
@@ -0,0 +1,21 @@
+// age = 24;
+
+// if(age >= 18){
+//     console.log("vote dite chole jao");
+    
+// }else {
+//     console.log("tmi boro hou age khuka/khuki");
+    
+// }
+
+// shortend ternery operator 
+// age = 21;
+// age >= 16 ? console.log("you go to voting point") : console.log("you sleeping now ,next time you go");
+
+score = 80;
+score >= 80 ? console.log("you got : A+") : console.log("you don't got : A+");
+
+price = 10;
+
+price ? console.log("you eat everything") : console.log("you are not available for this time ") ;
+
