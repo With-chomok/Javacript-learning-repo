7e042091e6b0ea99c2d011d3651dd3f5d2da5009
Chomok Das <183892208+With-chomok@users.noreply.github.com>
2025-07-29 13:19:28 +0600

    BMI calculate practice

---
+++ practice task/FreeDrinks.js
@@ -0,0 +1,16 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke : 30tk
+*/
+const coke = 30;
+const Burger = 600;
+
+if (Burger > 500){
+    console.log("free Coke");
+    
+}else {
+    console.log("you don't get coke free");
+}
+
