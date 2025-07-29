1f5247337c7fdffb565d7e008c0afa4c60061844
Chomok Das <183892208+With-chomok@users.noreply.github.com>
2025-07-29 08:37:06 +0600

    grade point uploads

--- multi.js
+++ multi.js
@@ -23,3 +23,44 @@ else {
     console.log(price);
     
 }
+
+
+// rastaurant tasks 
+age =60;
+
+if (age < 12){
+    console.log("He eat free");
+}else if(age < 20 && age > 12) {
+    console.log("he eat rice but don't get extra service");
+}else if (age < 40 && age > 20) {
+    console.log("He get All service this Restaurent");
+}else {
+    console.log("you can go because you not select.");
+}
+
+
+// grade point 
+
+mark = 93;
+
+if(mark >= 80){
+    console.log("A+");
+}
+else if (mark >= 70 && mark < 80){
+    console.log("A");
+}
+else if (mark >= 60 && mark < 70){
+    console.log("A");
+}
+else if (mark >= 50 && mark < 60){
+    console.log("B");
+}
+else if (mark >= 40 && mark < 50){
+    console.log("C");
+    
+}else if (mark >= 33 && mark < 40) {
+    console.log("D");
+    
+}else {
+    console.log("You are fail");
+}
