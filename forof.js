0c290d2ebfb1595fa4f57cd33637b677c876e3c7
Chomok Das <183892208+With-chomok@users.noreply.github.com>
2025-07-31 14:10:09 +0600

    while loop and do while loop

---
+++ forof.js
@@ -0,0 +1,14 @@
+// const numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 34, 34, 23, 56];
+// for (const number of numbers){
+//     console.log(number);
+    
+
+// }
+
+
+// let i =0;
+// while (i <= 10000){
+//     console.log(i);
+//     i++
+// }
+
