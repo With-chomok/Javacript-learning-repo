1f98a3493e4986e0e0853a372ca4f62229853c34
Chomok Das <183892208+With-chomok@users.noreply.github.com>
2025-08-06 20:24:44 +0600

    for and sum js

---
+++ sum.js
@@ -0,0 +1,41 @@
+// let num = 1;
+// let sum = 0;
+// // focus to condition 
+// while (num < 50){
+//     console.log(num);
+//     sum = sum + num;
+//     console.log("sum:" , sum);
+    
+//     num++
+// }
+
+// let num = 1;
+// while (num < 20){
+    
+//     if(num % 2 === 0){
+//         console.log("Even number is:-", num);
+//     }else{
+//         console.log("Odd number is:-", num);
+//     }
+//     num++
+    
+// }
+
+// let num = 1;
+// while (num <= 15){
+    
+//     if(num % 2 != 0){
+//         console.log("this is odd number:-", num);
+        
+//     }else{
+//         console.log("this is Even number:-", num);
+        
+//     }
+//     num++
+    
+// }
+
+for(let num = 1; num <= 20; num++){
+    console.log(num);
+    
+}
